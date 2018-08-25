f3d501f68df8b97a86a22a36b606710f9049be51
CordeliaLopez <corde.lpz@gmail.com>
2018-08-25 12:54:42 -0500

    master

---
+++ carpoolApp/Pods/Target Support Files/Pods-carpoolApp/Pods-carpoolApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_carpoolApp : NSObject
+@end
+@implementation PodsDummy_Pods_carpoolApp
+@end
---
+++ carpoolApp/carpoolApp/Base.lproj/DataViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DataViewController.swift
+//  carpoolApp
+//
+//  Created by Corde Lopez on 8/25/18.
+//  Copyright © 2018 Corde Lopez. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.setHidesBackButton(true, animated:true);
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
